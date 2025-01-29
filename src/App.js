2b3705d377f4c1029c1c46ad7b93b4a5a4234d02
Tuan Minh <tmminhdt.mpc@gmail.com>
2025-01-30 01:13:54 +0700

    xinn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './Layouts/Home';
-import LayoutDefault from './Layouts/LayoutDefault';
-import Game from './Layouts/Game';
-import Card1 from './Layouts/Card1';
-import Card2 from './Layouts/Card2';
-import Leaderboard from './Layouts/Leaderboard';
+import React, { useEffect, useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./Layouts/Home";
+import LayoutDefault from "./Layouts/LayoutDefault";
+import Game from "./Layouts/Game";
+import Card1 from "./Layouts/Card1";
+import Card2 from "./Layouts/Card2";
+import Leaderboard from "./Layouts/Leaderboard";
 
 function App() {
+  const location = useLocation(); 
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<LayoutDefault />}>
-          <Route path="/" element={<Home />} />
+        {/* Nếu đang ở /game và là mobile => Không render LayoutDefault */}
+        {location.pathname === "/game" && isMobile ? (
           <Route path="/game" element={<Game />} />
-          <Route path="/card1" element={<Card1 />} />
-          <Route path="/card2" element={<Card2 />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-        </Route>
+        ) : (
+          <Route path="/" element={<LayoutDefault />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/game" element={<Game />} />
+            <Route path="/card1" element={<Card1 />} />
+            <Route path="/card2" element={<Card2 />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+          </Route>
+        )}
       </Routes>
     </>
   );
